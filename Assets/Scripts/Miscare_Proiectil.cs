9a26cb736dd21d13d092d6c46912c7c64e78012a
georgemachis <george.machis@student.unitbv.ro>
2021-12-09 02:01:32 +0200

    Update Miscare_Proiectil.cs

--- Assets/Scripts/Miscare_Proiectil.cs
+++ Assets/Scripts/Miscare_Proiectil.cs
@@ -26,6 +26,7 @@ public class Miscare_Proiectil : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Enemy_Life.player1Thread > Enemy_Life.player2Thread * 3) { 
         transform.position = Vector2.MoveTowards(transform.position, target1, speed * Time.deltaTime);
         if (transform.position.x == target1.x && transform.position.y == target1.y)
         {
